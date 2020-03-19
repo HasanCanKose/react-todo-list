e256b9d86750ded624ad37a2be2fde14b1b59662
Hasan Can Kose <hasancannkosee@gmail.com>
2020-03-20 00:34:10 +0300

    Updates

---
+++ precache-manifest.7b01396ab60f432c32274ba558b7f978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f866c005020070eacd1647436ba9538",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "83236ff8721dad3ccb5c",
+    "url": "/react-todo-list/static/css/main.297e1f96.chunk.css"
+  },
+  {
+    "revision": "d3b6c59845f3d8516078",
+    "url": "/react-todo-list/static/js/2.818fc397.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-todo-list/static/js/2.818fc397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83236ff8721dad3ccb5c",
+    "url": "/react-todo-list/static/js/main.cdd74b6d.chunk.js"
+  },
+  {
+    "revision": "e45bd4270d25720027f4",
+    "url": "/react-todo-list/static/js/runtime-main.c10a6dca.js"
+  }
+]);
